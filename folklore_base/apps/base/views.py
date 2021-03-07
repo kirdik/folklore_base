f9f07b162d51c3f43000fb351c09f49f19907691
Кирилл Чеботарев <kirdik@me.com>
2021-03-07 15:58:08 +0300

    Добавлен постраничный вывод на страницу списка информантов

--- folklore_base/apps/base/views.py
+++ folklore_base/apps/base/views.py
@@ -48,7 +48,29 @@ def matherials(request, id):
 
 def informants(request):
     inf = Informant.objects.all()
-    return render(request, 'informants.html', {'inf': inf})
+    paginator = Paginator(inf, 8)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = { 'inf': page,
+                'is_paginated': is_paginated,
+                'next_url': next_url,
+                'prev_url': prev_url
+
+    }
+    return render(request, 'informants.html', context)
+
 def informant_details(request, id):
     infdetail = Informant.objects.get(id=id)
     seances = SeansOfRecord.objects.select_related().filter(informant_of_seanse=id)
